d159ed7278bc79529ebda8989be18cce027e8b09
Dan Kalinin <daniil5511@gmail.com>
2017-02-14 19:19:01 +0300

    tab control - constants -> anchors

--- Framework/Framework/TabControl.m
+++ Framework/Framework/TabControl.m
@@ -56,8 +56,10 @@
     NSTimeInterval duration = 0.25 * animated;
     [self.underscoreView.superview layoutIfNeeded];
     [UIView animateWithDuration:duration animations:^{
-        self.leadingConstraint.constant = button.frame.origin.x;
-        self.widthConstraint.constant = button.frame.size.width;
+        self.leadingConstraint.active = self.widthConstraint.active = NO;
+        self.leadingConstraint = [self.underscoreView.leadingAnchor constraintEqualToAnchor:button.leadingAnchor];
+        self.widthConstraint = [self.underscoreView.widthAnchor constraintEqualToAnchor:button.widthAnchor];
+        self.leadingConstraint.active = self.widthConstraint.active = YES;
         [self.underscoreView.superview layoutIfNeeded];
     }];
     
