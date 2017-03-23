e4dd536cdb0615ebc2e2232c284545d541e8948b
DanKalinin <daniil5511@gmail.com>
2017-03-23 15:58:01 +0300

    added cell outlets

--- Framework/Framework/TableView.h
+++ Framework/Framework/TableView.h
@@ -98,6 +98,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *label1;
 @property (weak, nonatomic) IBOutlet UILabel *label2;
 @property (weak, nonatomic) IBOutlet UILabel *label3;
+@property (weak, nonatomic) IBOutlet UILabel *label4;
+@property (weak, nonatomic) IBOutlet UILabel *label5;
 
 @property (weak, nonatomic) IBOutlet UITextField *textField1;
 
