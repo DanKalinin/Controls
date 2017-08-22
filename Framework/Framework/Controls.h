e713142584363364c0442b8132dc073d7479ba4d
Dan Kalinin <daniil551@hotmail.com>
2017-08-22 13:26:08 +0300

    Header

--- Framework/Framework/Controls.h
+++ Framework/Framework/Controls.h
@@ -6,15 +6,11 @@
 //  Copyright © 2016 Dan Kalinin. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for Framework.
 FOUNDATION_EXPORT double ControlsVersionNumber;
-
-//! Project version string for Framework.
 FOUNDATION_EXPORT const unsigned char ControlsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
 #import <Controls/ConcentricView.h>
 #import <Controls/TabControl.h>
 #import <Controls/GradientSlider.h>
