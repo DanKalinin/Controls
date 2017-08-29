db95328c901ce10ab62260dea0ab855d96dc7f31
Dan Kalinin <daniil551@hotmail.com>
2017-08-29 14:02:19 +0300

    Outlets

--- Framework/Framework/CollectionView.h
+++ Framework/Framework/CollectionView.h
@@ -56,9 +56,17 @@
 
 @property (weak, nonatomic) IBOutlet ShapeLayerView *shapeLayerView1;
 
+@property (weak, nonatomic) IBOutlet UIStackView *stackView1;
+
 @property (weak, nonatomic) IBOutlet UILabel *label1;
 @property (weak, nonatomic) IBOutlet UILabel *label2;
 @property (weak, nonatomic) IBOutlet UILabel *label3;
+@property (weak, nonatomic) IBOutlet UILabel *label4;
+@property (weak, nonatomic) IBOutlet UILabel *label5;
+@property (weak, nonatomic) IBOutlet UILabel *label6;
+@property (weak, nonatomic) IBOutlet UILabel *label7;
+@property (weak, nonatomic) IBOutlet UILabel *label8;
+@property (weak, nonatomic) IBOutlet UILabel *label9;
 
 @property (weak, nonatomic) IBOutlet Button *button1;
 
