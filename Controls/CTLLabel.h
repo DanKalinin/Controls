173b8a72f7df5c03192535074b4ae6b262f73cae
Dan Kalinin <daniil551@hotmail.com>
2018-08-25 20:38:41 +0300

    Border color

--- Controls/CTLLabel.h
+++ Controls/CTLLabel.h
@@ -6,11 +6,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CTLView.h"
 
 @class CTLLabel;
 
 
 
 @interface CTLLabel : UILabel
+
+@property IBInspectable UIColor *defaultBackgroundColor;
+@property IBInspectable UIColor *highlightedBackgroundColor;
+@property IBInspectable UIColor *disabledBackgroundColor;
+
+@property IBInspectable UIColor *defaultLayerBorderColor;
+@property IBInspectable UIColor *highlightedLayerBorderColor;
+@property IBInspectable UIColor *disabledLayerBorderColor;
 
 @end
--- Controls/CTLLabel.m
+++ Controls/CTLLabel.m
@@ -17,4 +17,28 @@
 
 @implementation CTLLabel
 
+- (void)setEnabled:(BOOL)enabled {
+    [super setEnabled:enabled];
+    
+    if (enabled) {
+        self.backgroundColor = self.defaultBackgroundColor;
+        self.layerBorderColor = self.defaultLayerBorderColor;
+    } else {
+        self.backgroundColor = self.disabledBackgroundColor;
+        self.layerBorderColor = self.disabledLayerBorderColor;
+    }
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+    [super setHighlighted:highlighted];
+    
+    if (highlighted) {
+        self.backgroundColor = self.highlightedBackgroundColor;
+        self.layerBorderColor = self.highlightedLayerBorderColor;
+    } else {
+        self.backgroundColor = self.defaultBackgroundColor;
+        self.layerBorderColor = self.defaultLayerBorderColor;
+    }
+}
+
 @end
