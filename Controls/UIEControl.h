386046db857b57c5cbc603e7c6bb635ff6d93c0c
Dan Kalinin <daniil551@hotmail.com>
2018-11-18 03:19:34 +0300

    Events

--- Controls/UIEControl.h
+++ Controls/UIEControl.h
@@ -7,7 +7,32 @@
 
 #import <Helpers/Helpers.h>
 
+@class UIEControlActionInfo;
 @class UIEControl;
+
+
+
+
+
+
+
+
+
+
+@interface UIEControlActionInfo : HLPObject
+
+@property (readonly) UIEvent *event;
+
+- (instancetype)initWithEvent:(UIEvent *)event;
+
+@end
+
+
+
+
+
+
+
 
 
 
@@ -17,6 +42,18 @@
 - (void)UIControlEventTouchDown:(UIEControl *)control;
 - (void)UIControlEventTouchDownRepeat:(UIEControl *)control;
 - (void)UIControlEventTouchDragInside:(UIEControl *)control;
+- (void)UIControlEventTouchDragOutside:(UIEControl *)control;
+- (void)UIControlEventTouchDragEnter:(UIEControl *)control;
+- (void)UIControlEventTouchDragExit:(UIEControl *)control;
+- (void)UIControlEventTouchUpInside:(UIEControl *)control;
+- (void)UIControlEventTouchUpOutside:(UIEControl *)control;
+- (void)UIControlEventTouchCancel:(UIEControl *)control;
+- (void)UIControlEventValueChanged:(UIEControl *)control;
+- (void)UIControlEventPrimaryActionTriggered:(UIEControl *)control;
+- (void)UIControlEventEditingDidBegin:(UIEControl *)control;
+- (void)UIControlEventEditingChanged:(UIEControl *)control;
+- (void)UIControlEventEditingDidEnd:(UIEControl *)control;
+- (void)UIControlEventEditingDidEndOnExit:(UIEControl *)control;
 
 @end
 
@@ -24,7 +61,10 @@
 
 @interface UIEControl : NSEOperation <UIEControlDelegate>
 
+@property (readonly) HLPArray<UIEControlDelegate> *delegates;
 @property (readonly) UIControl *control;
+@property (readonly) UIEControlActionInfo *touchDownInfo;
+@property (readonly) UIEControlActionInfo *touchDownRepeatInfo;
 
 - (instancetype)initWithControl:(UIControl *)control;
 
--- Controls/UIEControl.m
+++ Controls/UIEControl.m
@@ -9,9 +9,47 @@
 
 
 
+
+
+
+
+
+
+
+@interface UIEControlActionInfo ()
+
+@property UIEvent *event;
+
+@end
+
+
+
+@implementation UIEControlActionInfo
+
+- (instancetype)initWithEvent:(UIEvent *)event {
+    self = super.init;
+    if (self) {
+        self.event = event;
+    }
+    return self;
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
 @interface UIEControl ()
 
 @property UIControl *control;
+@property UIEControlActionInfo *touchDownInfo;
+@property UIEControlActionInfo *touchDownRepeatInfo;
 
 @end
 
@@ -19,12 +57,43 @@
 
 @implementation UIEControl
 
+@dynamic delegates;
+
 - (instancetype)initWithControl:(UIControl *)control {
     self = super.init;
     if (self) {
         self.control = control;
+        
+        [self.control addTarget:self action:@selector(UIControlEventTouchDown:event:) forControlEvents:UIControlEventTouchDown];
+        [self.control addTarget:self action:@selector(UIControlEventTouchDownRepeat:event:) forControlEvents:UIControlEventTouchDownRepeat];
     }
     return self;
 }
+
+#pragma mark - Actions
+
+- (void)UIControlEventTouchDown:(UIControl *)control event:(UIEvent *)event {
+    self.touchDownInfo = [UIEControlActionInfo.alloc initWithEvent:event];
+    [self.delegates UIControlEventTouchDown:self];
+}
+
+- (void)UIControlEventTouchDownRepeat:(UIEControl *)control event:(UIEvent *)event {
+    self.touchDownRepeatInfo = [UIEControlActionInfo.alloc initWithEvent:event];
+    [self.delegates UIControlEventTouchDownRepeat:self];
+}
+
+//- (void)UIControlEventTouchDragInside:(UIEControl *)control;
+//- (void)UIControlEventTouchDragOutside:(UIEControl *)control;
+//- (void)UIControlEventTouchDragEnter:(UIEControl *)control;
+//- (void)UIControlEventTouchDragExit:(UIEControl *)control;
+//- (void)UIControlEventTouchUpInside:(UIEControl *)control;
+//- (void)UIControlEventTouchUpOutside:(UIEControl *)control;
+//- (void)UIControlEventTouchCancel:(UIEControl *)control;
+//- (void)UIControlEventValueChanged:(UIEControl *)control;
+//- (void)UIControlEventPrimaryActionTriggered:(UIEControl *)control;
+//- (void)UIControlEventEditingDidBegin:(UIEControl *)control;
+//- (void)UIControlEventEditingChanged:(UIEControl *)control;
+//- (void)UIControlEventEditingDidEnd:(UIEControl *)control;
+//- (void)UIControlEventEditingDidEndOnExit:(UIEControl *)control;
 
 @end
