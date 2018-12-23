39f29a2dda7903799fd7b801bf7bf5dbd9348bd3
Dan Kalinin <daniil551@hotmail.com>
2018-12-23 23:28:47 +0300

    UITextField

--- Controls/UITextField.h
+++ Controls/UITextField.h
@@ -8,16 +8,61 @@
 #import <UIKit/UIKit.h>
 #import "UIControl.h"
 
+@class UITextFieldOperationShouldChangeInfo;
+@class UITextFieldOperationShouldReturnInfo;
 @class UITextFieldOperation;
 
 @protocol UITextFieldOperationDelegate;
 
 
 
+
+
+
+
+
+
+
+@interface UITextFieldOperationShouldChangeInfo : HLPObject
+
+@property BOOL shouldChange;
+
+@property (readonly) NSRange range;
+@property (readonly) NSString *string;
+
+- (instancetype)initWithRange:(NSRange)range string:(NSString *)string;
+
+@end
+
+
+
+
+
+
+
+
+
+
+@interface UITextFieldOperationShouldReturnInfo : HLPObject
+
+@property BOOL shouldReturn;
+
+@end
+
+
+
+
+
+
+
+
+
+
 @protocol UITextFieldOperationDelegate <UIControlOperationDelegate, UITextFieldDelegate>
 
 @optional
-- (void)UITextFieldOperationShouldChangeCharacters:(UITextFieldOperation *)operation;
+- (void)UITextFieldOperationDidBeginEditing:(UITextFieldOperation *)operation;
+- (void)UITextFieldOperationShouldChange:(UITextFieldOperation *)operation;
 - (void)UITextFieldOperationShouldReturn:(UITextFieldOperation *)operation;
 
 @end
@@ -28,7 +73,25 @@
 
 @property (readonly) HLPArray<UITextFieldOperationDelegate> *delegates;
 @property (readonly) UITextField *object;
+@property (readonly) UITextFieldOperationShouldChangeInfo *shouldChangeInfo;
+@property (readonly) UITextFieldOperationShouldReturnInfo *shouldReturnInfo;
 
 @property (weak, readonly) UITextField *weakObject;
+
+@end
+
+
+
+
+
+
+
+
+
+
+@interface UITextField (UIE)
+
+@property IBInspectable BOOL clearOnBegin;
+@property IBInspectable NSString *pattern;
 
 @end
--- Controls/UITextField.m
+++ Controls/UITextField.m
@@ -9,7 +9,77 @@
 
 
 
+
+
+
+
+
+
+
+@interface UITextFieldOperationShouldChangeInfo ()
+
+@property NSRange range;
+@property NSString *string;
+
+@end
+
+
+
+@implementation UITextFieldOperationShouldChangeInfo
+
+- (instancetype)initWithRange:(NSRange)range string:(NSString *)string {
+    self = super.init;
+    if (self) {
+        self.range = range;
+        self.string = string;
+        
+        self.shouldChange = YES;
+    }
+    return self;
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+@interface UITextFieldOperationShouldReturnInfo ()
+
+@end
+
+
+
+@implementation UITextFieldOperationShouldReturnInfo
+
+- (instancetype)init {
+    self = super.init;
+    if (self) {
+        self.shouldReturn = YES;
+    }
+    return self;
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
 @interface UITextFieldOperation ()
+
+@property UITextFieldOperationShouldChangeInfo *shouldChangeInfo;
+@property UITextFieldOperationShouldReturnInfo *shouldReturnInfo;
 
 @end
 
@@ -28,5 +98,81 @@
     }
     return self;
 }
+
+#pragma mark - Text field
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [self.delegates UITextFieldOperationDidBeginEditing:self];
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    self.shouldChangeInfo = [UITextFieldOperationShouldChangeInfo.alloc initWithRange:range string:string];
+    [self.delegates UITextFieldOperationShouldChange:self];
+    return self.shouldChangeInfo.shouldChange;
+}
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField {
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    self.shouldReturnInfo = UITextFieldOperationShouldReturnInfo.new;
+    [self.delegates UITextFieldOperationShouldReturn:self];
+    return self.shouldReturnInfo.shouldReturn;
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    
+}
+
+#pragma mark - Text field operation
+
+- (void)UITextFieldOperationDidBeginEditing:(UITextFieldOperation *)operation {
+    if (self.weakObject.clearOnBegin) {
+        self.weakObject.text = @"";
+    }
+}
+
+- (void)UITextFieldOperationShouldChange:(UITextFieldOperation *)operation {
+    if (self.weakObject.pattern.length > 0) {
+        NSString *text = [self.weakObject.text stringByReplacingCharactersInRange:self.shouldChangeInfo.range withString:self.shouldChangeInfo.string];
+        NSRange range = [text rangeOfString:self.weakObject.pattern options:NSRegularExpressionSearch];
+        if (range.location == NSNotFound) {
+            self.shouldChangeInfo.shouldChange = NO;
+        } else {
+            self.shouldChangeInfo.shouldChange = YES;
+        }
+    } else {
+        self.shouldChangeInfo.shouldChange = YES;
+    }
+}
+
+- (void)UITextFieldOperationShouldReturn:(UITextFieldOperation *)operation {
+    
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+@implementation UITextField (UIE)
+
+@dynamic clearOnBegin;
+@dynamic pattern;
 
 @end
