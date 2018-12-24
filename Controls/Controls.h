f88d811165f5498e4ebcf65542e21728b5b9448c
Dan Kalinin <daniil551@hotmail.com>
2018-12-24 17:46:04 +0300

    Header

--- Controls/Controls.h
+++ Controls/Controls.h
@@ -15,12 +15,14 @@ FOUNDATION_EXPORT const unsigned char ControlsVersionString[];
 #import <Controls/UIView.h>
 #import <Controls/UIWindow.h>
 #import <Controls/UIControl.h>
+#import <Controls/UIButton.h>
 #import <Controls/UITextField.h>
 #import <Controls/UIViewController.h>
 
 #import <Controls/UIEApplication.h>
 #import <Controls/UIEWindow.h>
 #import <Controls/UIEControl.h>
+#import <Controls/UIEButton.h>
 #import <Controls/UIETextField.h>
 #import <Controls/UIEAction.h>
 #import <Controls/UIEAlert.h>
@@ -34,7 +36,6 @@ FOUNDATION_EXPORT const unsigned char ControlsVersionString[];
 #import <Controls/UIENavigationController.h>
 #import <Controls/UIETabBar.h>
 #import <Controls/UIETabBarController.h>
-#import <Controls/UIEButton.h>
 #import <Controls/UIEStoryboardSegue.h>
 
 #import <Controls/CTLApplication.h>
