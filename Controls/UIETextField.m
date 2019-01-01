14d4b0ed62906ec35e244dfc30f2d211d0bbce93
Dan Kalinin <daniil551@hotmail.com>
2019-01-02 01:59:14 +0300

    Control

--- Controls/UIETextField.m
+++ Controls/UIETextField.m
@@ -19,6 +19,8 @@
 @implementation UITextField (UIE)
 
 @dynamic nseOperation;
+@dynamic uiePattern;
+@dynamic uieClearOnBegin;
 
 - (Class)nseOperationClass {
     return UIETextFieldOperation.class;
@@ -66,5 +68,39 @@
 @implementation UIETextFieldOperation
 
 @dynamic object;
+
+- (instancetype)initWithObject:(UITextField *)object {
+    self = [super initWithObject:object];
+    
+    object.delegate = self;
+    
+    return self;
+}
+
+- (void)editingDidBegin:(UITextField *)sender event:(UIEvent *)event {
+    [super editingDidBegin:sender event:event];
+    
+    [self.delegates uieControlEditingDidBegin:sender];
+}
+
+- (void)editingChanged:(UITextField *)sender event:(UIEvent *)event {
+    [super editingChanged:sender event:event];
+    
+    [self.delegates uieControlEditingChanged:sender];
+}
+
+- (void)editingDidEnd:(UITextField *)sender event:(UIEvent *)event {
+    [super editingDidEnd:sender event:event];
+    
+    [self.delegates uieControlEditingDidEnd:sender];
+}
+
+- (void)editingDidEndOnExit:(UITextField *)sender event:(UIEvent *)event {
+    [super editingDidEndOnExit:sender event:event];
+    
+    [self.delegates uieControlEditingDidEndOnExit:sender];
+}
+
+#pragma mark - UITextFieldDelegate
 
 @end
